0560c8aaa561620212d68e9889c1617ae201026e
Ken Duigraha Putra <kenduigraha@gmail.com>
2025-02-04 11:45:06 +0700

    Remove sb mainjs for the default and add vercel.json to serve public folder for sb.

--- .storybook/main.js
+++ .storybook/main.js
@@ -12,6 +12,6 @@ const config = {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  staticDirs: ["..\\public", "..\\storybook-static"],
+  staticDirs: ["..\\public"],
 };
 export default config;
