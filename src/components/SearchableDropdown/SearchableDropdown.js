e719afd0dd8c259ac7581dee66b0b2ab2e80346b
Ken Duigraha Putra <kenduigraha@gmail.com>
2025-02-04 17:29:40 +0700

    Add highlight search feature.

--- src/components/SearchableDropdown/SearchableDropdown.js
+++ src/components/SearchableDropdown/SearchableDropdown.js
@@ -1,10 +1,15 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { motion } from "framer-motion";
 import SelectedItem from "../SelectedItem";
+
+// helper
+const findWord = (word, str) => {
+    return str.includes(word);
+};
   
 function SearchableDropdown({
-    id = 'sdd-1',
+    id = '1',
     options = [],
     multiple = true,
     withSearch = true,
@@ -14,38 +19,50 @@ function SearchableDropdown({
     onChange = () => {},
   }) {
     const [open, setOpen] = useState(false);
+	const [optionList, setOptionList] = useState([]);
 	const [searchInput, setSearchInput] = useState("");
+    const [debouncedInputValue, setDebouncedInputValue] = useState("");
 	const [selectedData, setselectedData] = useState([]);
 	const [selectedDataSet, setselectedDataSet] = useState(new Set());
 	const [activeSuggestion, setActiveSuggestion] = useState(0);
 
 	const inputRef = useRef(null);
-    
 
+    useEffect(() => {
+        const delayInputTimeoutId = setTimeout(() => {
+          setDebouncedInputValue(searchInput);
+        }, 500);
+        return () => clearTimeout(delayInputTimeoutId);
+      }, [searchInput, 500]);
+
+    useEffect(() => {
+        setOptionList(options);
+    }, [options]);
+    
     const DropdownList = (
         <motion.ul
+            key={`dropdown-list-${id}`}
             className="absolute mt-1 w-full bg-white border border-gray-300 rounded-lg shadow-lg max-h-60 overflow-auto z-[1050] suggestions-list"
             initial={{ opacity: 0, scale: 0.95 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.95 }}
         >
-            {options?.map((data, index) => {
-                // TODO: selected item background highligth
+            {optionList.length > 0 && optionList?.map((data, index) => {
                 return !selectedDataSet.has(data.label) ? (
                     <li
+                        key={`option-item-${data.id}-${index}`}
                         className={`p-2 cursor-pointer hover:bg-gray-100 transition-colors ${
                             selectedData === data
                             ? "bg-blue-500 text-white"
                             : ""
                         }`}
-                        key={data.id || data.value}
                         onClick={() => {
                             handleSelectData(data)
                             setOpen(!open)
                         }}
                     >
                         <span>
-                            {data.label} {data.icon}
+                            {data.parsedLabel ? (data.parsedLabel) : `${data.label}`} {data.icon}
                         </span>
                     </li>
                 ) : (
@@ -57,13 +74,46 @@ function SearchableDropdown({
 
     const handleOnChange = (e) => {
         e.preventDefault();
-
-        setOpen(!open);
+        const textValue = e.target.value.trim();
         
-        setSearchInput(e.target.value);
+        setSearchInput(textValue);
 
+        if (!textValue) {
+            setOpen(false);
+
+            setOptionList(options);
+
+            return;
+        }
+        const dataOptions = [];
+        // highlight words
+        optionList.map((option) => {
+            const dataItemOption = option.label.toLowerCase();
+            const indexSearchedText = dataItemOption.indexOf(textValue);
+            const isFounded = findWord(textValue, dataItemOption);
+
+            const newElement = [
+                dataItemOption.substring(0, indexSearchedText),
+                <span className={'bg-highlight'}>
+                    {option.label.substring(indexSearchedText, indexSearchedText + textValue.length)}
+                </span>,
+                option.label.substring(indexSearchedText + textValue.length)
+            ];
+
+            dataOptions.push({
+                ...option,
+                parsedLabel: isFounded ? newElement : null,
+            });
+            
+            setOptionList([...dataOptions]);
+        });
+
+        setOpen(true);
+
+        // from parent component
         onChange(e);
-    }
+    };
+    
 	const handleSelectData = (data) => {
         if (multiple) {
             // prevent double selection
@@ -98,7 +148,6 @@ function SearchableDropdown({
 		setselectedDataSet(updatedData);
 	};
 
-
     // TODO: for better UX
 	const handleKeyDown = (e) => {
 		if (
@@ -109,30 +158,30 @@ function SearchableDropdown({
 			const lastData = selectedData[selectedData.length - 1];
 			handleRemoveData(lastData);
 		} else if (e.key === "ArrowDown" &&
-			options?.label?.length > 0) {
+			optionList?.label?.length > 0) {
 			e.preventDefault();
 			setActiveSuggestion((prevIndex) =>
-				prevIndex < options.label.length - 1 ?
+				prevIndex < optionList.label.length - 1 ?
 					prevIndex + 1 : prevIndex
 			);
 		} else if (e.key === "ArrowUp" &&
-			options?.label?.length > 0) {
+			optionList?.label?.length > 0) {
 			e.preventDefault();
 			setActiveSuggestion((prevIndex) =>
 				(prevIndex > 0 ? prevIndex - 1 : 0));
 		} else if (
 			e.key === "Enter" &&
 			activeSuggestion >= 0 &&
-			activeSuggestion < options.label.length
+			activeSuggestion < optionList.label.length
 		) {
-			handleSelectData(options.label[activeSuggestion]);
+			handleSelectData(optionList.label[activeSuggestion]);
 		}
 	};
 
 	return (
-		<div key={id} className="user-search-container">
+		<div key={`search-dropdown-container-${id}`} className="user-search-container">
             {optionLabel && (
-                <label for={optionLabel}>
+                <label htmlFor={optionLabel}>
                     {optionLabel}
                 </label>
             )}
